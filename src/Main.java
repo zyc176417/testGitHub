3ad0e7501119426bddd6f96b25c516879c27f545
zyc <1510176417@qq.com>
2022-05-31 19:35:39 +0800

    开发中提交

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("---项目上传测试！---");
+        System.out.println("---开发中！---");
     }
 }
